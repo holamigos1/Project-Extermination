6942dfcd7703557f6475f42edc6a4f49b958f8e7
DAcKeyy <lewaLOC@gmail.com>
2023-02-14 00:27:18 +0300

    refactoring: чишю вилкой конртоллеры персонажа и скрипты пушки

---
+++ Assets/Scripts/OtherСomponents/SourseMovment/Ladder.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    // Empty class for defining objects as ladders
+    // Probably inefficient, but I didn't want to use tags or layers
+}
